f49103c7c3d001e614f3d5a9e9a10ff2f492e409
Samuel A. <csanekwe@gmail.com>
2025-12-18 09:19:59 -0700

    Initial SOAP Notes MVP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("https://soap-notes-app.onrender.com/soap-notes-app", {
+    const response = await fetch("https://soap-notes-app.onrender.com/soap-notes", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
